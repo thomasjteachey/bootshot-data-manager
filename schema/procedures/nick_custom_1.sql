0e6e7ae1bfbfdd17428b73c2b5a5e5b2ff2950dc
Thomas Teachey <tteachey@installationmadeeasy.com>
2026-07-07 11:24:11 -0400

    Revert "Merge pull request #5 from thomasjteachey/codex/organize-mysql-dump-into-subfolders"
    This reverts commit f5ef5d620575010275ed0a261ad139b2901cece5, reversing
    changes made to 34b07f43539ee48add9be608249e0f8795237779.

--- schema/procedures/nick_custom_1.sql
+++
@@ -1,6 +0,0 @@
-DELIMITER ;;
-CREATE DEFINER=`brokilodeluxe`@`%` PROCEDURE `nick_custom_1`()
-BEGIN
-select * from person;
-END ;;
-DELIMITER ;
--- schema/tables/household.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `household` (
-  `household_id` bigint unsigned NOT NULL,
-  `person_id` bigint unsigned NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`household_id`,`person_id`),
-  KEY `ix_household_person` (`person_id`),
-  CONSTRAINT `fk_household_person` FOREIGN KEY (`person_id`) REFERENCES `person` (`person_id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
