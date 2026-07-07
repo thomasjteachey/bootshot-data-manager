28dde549e2e49afc5b7f6603a5c0f42d61896300
Thomas Teachey <tteachey@installationmadeeasy.com>
2026-07-07 11:28:16 -0400

    Reapply "Merge pull request #5 from thomasjteachey/codex/organize-mysql-dump-into-subfolders"
    This reverts commit 0e6e7ae1bfbfdd17428b73c2b5a5e5b2ff2950dc.

---
+++ schema/procedures/nick_custom_1.sql
@@ -0,0 +1,6 @@
+DELIMITER ;;
+CREATE DEFINER=`brokilodeluxe`@`%` PROCEDURE `nick_custom_1`()
+BEGIN
+select * from person;
+END ;;
+DELIMITER ;
---
+++ schema/tables/clinic_dob_export.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `clinic_dob_export` (
+  `patientid` text,
+  `apptcancelleddate` text,
+  `apptcancelreason` text,
+  `APPTCANCELREASONTYP` text,
+  `apptcheckindate` text,
+  `apptconfresult` text,
+  `apptdate` text,
+  `apptrequirescancel` text,
+  `apptslotduration` text,
+  `appttype` text,
+  `appttypeclass` text
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
